a802dd33c828d84355de8bbcf8496e5a84ed6998
jom_mac <[john.a@portalink.com]>
2026-03-14 10:29:02 +0800

    adjusting time

--- helpers/Helpers.py
+++ helpers/Helpers.py
@@ -3,7 +3,7 @@ import time
 def clear_console():
     print('\033c', end='')
 
-def add_break_point(time_for_sleep=5, sleep_only = False):
+def add_break_point(time_for_sleep=7, sleep_only = False):
     if not sleep_only:
         clear_console()
     time.sleep(time_for_sleep)
